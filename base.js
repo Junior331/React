882db79a662d9360fd2555490567391a9cc936aa
Junior331 <qualquercoisa333133127@gmail.com>
2019-09-22 11:48:05 -0300

    aula2

--- base.js
+++ base.js
@@ -1,2 +1,13 @@
-let app = document.getElementById("app");
-ReactDOM.render('Olá Jaja', app);
+function mostrarNome(nome, sobrenome){
+    return (
+        <div>
+            <h1>{nome}</h1>
+            <p>{sobrenome}</p>
+            <small>Seja bem vindo</small>
+        </div>
+    );
+}
+ReactDOM.render(
+    mostrarNome('Jaja', 'rk'),
+    document.querySelector("#app")
+);
